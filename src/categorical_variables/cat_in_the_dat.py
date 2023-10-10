b42665083818d690ef1e4a5ae0af9e1ba3ab5124
Leonard <15871497508@163.com>
2023-10-10 18:41:21 +0800

    cat_in_the_dat v2

--- src/categorical_variables/cat_in_the_dat.py
+++ src/categorical_variables/cat_in_the_dat.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import argparse
-from sklearn import preprocessing, model_selection, linear_model, metrics
+from sklearn import preprocessing, model_selection, linear_model, metrics, tree, ensemble
 import config
+import os
+import matplotlib.pyplot as plt
 
 def explore_data(dataframe):
     """Use feature dataframe, print feature's infomation"""
@@ -37,30 +39,61 @@ def preprocess(dataframe_all, fill_na=True, select_fill_na_feature=None,
             print("Error: select_rare_feature should be None or list type")
             return -1
     if categorical_encode is not None:
+        # check categorical_encode
+        if categorical_encode not in config.encode_dict:
+            print("Error: {} is not in config.encode_dict".format(categorical_encode))
+            return -1
+        
         # encode for all features if select_categorical_encode is None
-        if (select_categorical_encode is None) or (type(select_categorical_encode) is list):
-            columns = dataframe_all.columns if select_categorical_encode is None else select_categorical_encode
-            # check categorical_encode
-            if categorical_encode not in config.encode_dict:
-                print("Error: {} is not in config.encode_dict".format(categorical_encode))
-                return -1
-
-            # encoder fit_transform
-            encoder = config.encode_dict[categorical_encode]
+        encoder = config.encode_dict[categorical_encode]
+        if select_categorical_encode is None:
+            columns = dataframe_all.columns
             if categorical_encode == "OneHot":
                 ###### TODO: OneHotEncoder for selected features
                 encoder.fit(dataframe_all)
             else:
                 for col in columns:
                     dataframe_all.loc[:, col] = encoder.fit_transform(dataframe_all.loc[:, col])
+        elif type(select_categorical_encode) in (list, str):
+            columns = select_categorical_encode if type(select_categorical_encode) is list else [select_categorical_encode]
+            if categorical_encode == "OneHot":
+                encoder.sparse_output = False
+                encodings = encoder.fit_transform(dataframe_all[columns])
+                dataframe_all[encoder.get_feature_names_out()] = encodings
+                dataframe_all = dataframe_all.drop(columns=columns)
+            else:
+                for col in columns:
+                    dataframe_all.loc[:, col] = encoder.fit_transform(dataframe_all.loc[:, col])
+        else:
+            print("Error: select_categorical_encode should be None or list type")
+            return -1
 
     return dataframe_all, encoder
+
+def write_prediction_csv(fold_num, model, df_test_id, X_test):
+    df_results = pd.DataFrame(df_test_id)
+    df_results["target"] = model.predict_proba(X_test)[:, 1]
+    df_results.to_csv(os.path.join(config.OUTPUT_CSV_ROOT_PATH, str(fold_num) + "_submission.csv"), index=False)
+
+def draw_roc_curve(Y, probability):
+    print("Draw Roc Curve...")
+    fpr, tpr, thresholds = metrics.roc_curve(Y, probability)
+    for index, (x, y, threshold) in enumerate(zip(fpr, tpr, thresholds)):
+        if index % 5000 == 0:
+            plt.text(x, y, round(threshold,2), fontsize=10,va='bottom',ha='center')
+    plt.plot(fpr, tpr, marker = 'o')
+    plt.xlim(0, 1.)
+    plt.ylim(0, 1.)
+    plt.xlabel('FPR', fontsize=15)
+    plt.ylabel('TPR', fontsize=15)
+    plt.show()
 
 if __name__ == "__main__":
     # explore data
     print("Reading csv files...")
-    df_labeled = pd.read_csv("../../input/cat_in_the_dat/train.csv")
-    df_test = pd.read_csv("../../input/cat_in_the_dat/test.csv")
+    df_labeled = pd.read_csv(config.TRAIN_CSV_PATH)
+    df_test = pd.read_csv(config.TEST_CSV_PATH)
+    df_test_id = df_test.id
     list_features = [feature for feature in df_labeled.columns if feature not in ["id", "target"]]
     # explore_data(dataframe=df_labeled[list_features])
 
@@ -73,22 +106,28 @@ if __name__ == "__main__":
     df_test = df_all.tail(len(df_test))
 
     # cross-validation
-    stratified_kfold = config.cross_validation_dict["StratifiedKFold"]  ##
+    stratified_kfold = config.cross_validation_dict["StratifiedKFold"]  ###
     for fold_index, (train_index, val_index) in enumerate(stratified_kfold.split(X=df_labeled, y=df_target)):
         print("Fold: {}".format(fold_index))
 
-        X_train = encoder.transform(df_labeled.loc[train_index, :])
-        Y_train = df_target.loc[train_index]
+        X_train = encoder.transform(df_labeled.loc[train_index, :])  # OneHotEncoder
         X_val = encoder.transform(df_labeled.loc[val_index, :])
+        # X_train = df_labeled.loc[train_index, :]
+        # X_val = df_labeled.loc[val_index, :]
+        Y_train = df_target.loc[train_index]
         Y_val = df_target.loc[val_index]
 
         print("Training...")
-        model = linear_model.LogisticRegression()
+        model = config.model_dict["LogisticRegression"]  ###
         model.fit(X_train, Y_train)
 
         print("Validation...")
+        train_probability = model.predict_proba(X_train)[:, 1]
         val_probability = model.predict_proba(X_val)[:, 1]
 
-        auc = metrics.roc_auc_score(Y_val, val_probability)
-        print("AUC: {}".format(auc))
+        train_auc = metrics.roc_auc_score(Y_train, train_probability)
+        val_auc = metrics.roc_auc_score(Y_val, val_probability)
+        print("Train_AUC: {}, Val_AUC: {}".format(train_auc, val_auc))
 
+        print("Saving...")
+        write_prediction_csv(fold_index, model, df_test_id, encoder.transform(df_test))
--- src/categorical_variables/config.py
+++ src/categorical_variables/config.py
@@ -1,8 +1,12 @@
-from sklearn import preprocessing, model_selection
+from sklearn import preprocessing, model_selection, ensemble, tree, linear_model
+import xgboost as xgb
 
-FOLD_NUM = 5
+FOLD_NUM = 600
 RARE_VALUE_COUNTS = 2000
 RARE_VALUE_RATIO = 0.03
+TRAIN_CSV_PATH = "../../input/cat_in_the_dat/train.csv"
+TEST_CSV_PATH = "../../input/cat_in_the_dat/test.csv"
+OUTPUT_CSV_ROOT_PATH = "../../output/logistic_regression"
 
 encode_dict = {
     "OneHot": preprocessing.OneHotEncoder(),
@@ -12,4 +16,11 @@ encode_dict = {
 cross_validation_dict = {
     "KFold": model_selection.KFold(n_splits=FOLD_NUM),
     "StratifiedKFold": model_selection.StratifiedKFold(n_splits=FOLD_NUM)
+}
+
+model_dict = {
+    "LogisticRegression": linear_model.LogisticRegression(penalty='l2', dual=False, tol=0.0001, C=1.0, max_iter=100),
+    "DecisionTreeClassifier": tree.DecisionTreeClassifier(criterion='gini', splitter='best', max_depth=10, min_samples_split=2),
+    "RandomForestClassifier": ensemble.RandomForestClassifier(n_estimators=100, criterion='gini', max_depth=None, n_jobs=-1),
+    "XGBClassifier": xgb.XGBClassifier(max_depth=7, n_estimators=200, n_jobs=-1)
 }
